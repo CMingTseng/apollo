7613c10330481237c65f7b22bba78b10947effb4
Gary Tierney <gary.tierney@gmx.com>
2017-12-31 07:31:22 +0000

    Add attribute delegates to the API plugin

--- game/plugin/api/src/org/apollo/game/plugins/api/util.kt
+++ game/plugin/api/src/org/apollo/game/plugins/api/util.kt
@@ -1,7 +1,21 @@
+import java.lang.ref.WeakReference
 import java.util.*
+import kotlin.reflect.KProperty
 
 val RAND = Random()
 
 fun rand(bounds: Int): Int {
     return RAND.nextInt(bounds)
+}
+
+class WeakRefHolder<T>(private var _value: WeakReference<T?> = WeakReference(null)) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T? {
+        return _value.get()
+    }
+
+    operator fun setValue(
+        thisRef: Any?, property: KProperty<*>, value: T?
+    ) {
+        _value = WeakReference(value)
+    }
 }
